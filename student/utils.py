b863438580fb91a9661b19f427f90625a51ec064
221260116056 <221260116056setiit@gmail.com>
2026-05-17 10:39:54 +0530

    bug fixing 4

--- student/utils.py
+++ student/utils.py
@@ -3,7 +3,8 @@ from io import BytesIO
 
 
 def generate_qr_code(verification_url):
-    qr = qrcode.make(verification_url)
+    from qrcode.image.pil import PilImage
+    qr = qrcode.make(verification_url, image_factory=PilImage)
     buffer = BytesIO()
     qr.save(buffer, format='PNG')
     buffer.seek(0)
